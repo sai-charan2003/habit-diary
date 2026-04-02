f9c5d0a3db61e97c9cb0dc6d6c9469c9ea56dad4
sai-charan2003 <saicharannallapaneni@gmail.com>
2026-04-02 12:27:17 -0400

    fix: date update issue in widget

--- app/src/main/java/com/charan/habitdiary/widgets/HabitDiaryWidgetManager.kt
+++ app/src/main/java/com/charan/habitdiary/widgets/HabitDiaryWidgetManager.kt
@@ -2,13 +2,8 @@ package com.charan.habitdiary.widgets
 
 import android.content.Context
 import androidx.glance.appwidget.updateAll
-import dagger.hilt.android.qualifiers.ApplicationContext
-import javax.inject.Inject
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 
-class HabitDiaryWidgetManager @Inject constructor(
+class HabitDiaryWidgetManager(
     private val context: Context
 ) {
 
